6b7a51cfa09e7a6000d3fe9aa0e6c17b10b24b36
Hao Zou <zou39@purdue.edu>
2017-05-24 12:09:24 -0400

    Sign Up page set up
    Set up the sign up page

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from . import views
+
+from polls import views as polls_views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', polls_views.polls,name='polls'),
 ]
