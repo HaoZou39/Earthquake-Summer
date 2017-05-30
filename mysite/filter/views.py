d583f0f9e22093f2ea6f94a07418fa88ffba53d8
Rohil Sharma <sharm183@purdue.edu>
2017-05-30 14:06:43 -0400

    Added first version of filtering app

---
+++ mysite/filter/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def filter(request):
+	return render(request, 'filterIndex.html')
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'homepage',
     'bootstrap3',
+	'filter',
 ]
 
 MIDDLEWARE = [
