1531910dd746aa419a7a9c84628517110fcac047
Rohil Sharma <sharm183@purdue.edu>
2017-06-15 14:27:48 -0400

    Added file validation; Users can only upload .jpg images

--- mysite/filter/models.py
+++ mysite/filter/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from django.core.exceptions import ValidationError
 
 def renameToPrimaryKey(instance, filename):
 	ext = filename.split('.')
@@ -7,6 +8,10 @@ def renameToPrimaryKey(instance, filename):
 		return 'filter/static/images/'+'{}.{}'.format(instance.pk,ext[1])
 	else:
 		return 'filter/static/images/DNE.' + ext[1]
+
+def validateFile(value):
+	if not value.name.endswith('.jpg'):
+		raise ValidationError(u'Error! Please use .jpg files only!')
 
 class camera(models.Model):
 	caseID = models.CharField(max_length=10) #Some case IDs are longer than 3 digits. Max length of 10 to be safe. Maybe remove this constraint?
@@ -18,7 +23,7 @@ class camera(models.Model):
 	totalFloorArea_m2 = models.PositiveIntegerField(default=0)
 	lastModifiedUser = models.CharField(max_length=140, default="")
 	lastModifiedDate = models.DateTimeField(auto_now=True)
-	photo = models.ImageField(upload_to=renameToPrimaryKey, default='static/images/'+str(datetime.now))
+	photo = models.ImageField(upload_to=renameToPrimaryKey, default='static/images/'+str(datetime.now), validators=[validateFile])
 
 	def __str__(self):
 		return self.caseID
